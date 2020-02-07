f12efd54362b09cd9fe42e1a04785163e86f5397
dagilapon <deykergil28@gmail.com>
2020-02-07 15:15:26 -0400

    Arreglo de Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(PermissionsTableSeeder::class);
-        //$this->call(CategoriesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
         $this->call(DependencesTableSeeder::class);
         $this->call(TypesTableSeeder::class);
     }
